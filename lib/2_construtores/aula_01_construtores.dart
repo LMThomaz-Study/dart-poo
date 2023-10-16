70982ea72b0bb5f1e598ef22ad374ff61c1a18c5
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-10-16 08:22:14 -0300

    constructors

---
+++ lib/2_construtores/aula_01_construtores.dart
@@ -0,0 +1,24 @@
+import 'package:dart_poo/2_construtores/pessoa.dart';
+
+void main() {
+  // var pessoa = Pessoa('Leonardo', 24,'Masculino');
+
+  // var pessoa = Pessoa(
+  //   idadeConstruct: 24,
+  //   nomeConstruct: 'Leonardo',
+  //   sexoConstruct: 'Masculino',
+  // );
+
+  var pessoa = Pessoa(
+    idade: 24,
+    nome: 'Leonardo',
+    sexo: 'Masculino',
+  );
+
+  var pessoaSemNome = Pessoa.semNome(
+    idade: 24,
+    sexo: 'Masculino',
+  );
+
+  var pessoaVazia = Pessoa.vazia();
+}
